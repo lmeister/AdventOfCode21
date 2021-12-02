33f491c1d7094120b1b98660c2a81659dd43bda3
Leon Meister <leonmeister97@gmail.com>
2021-12-02 09:22:05 +0100

    Add missing visibility

--- src/de/leonm/adventofcode21/days/Day.java
+++ src/de/leonm/adventofcode21/days/Day.java
@@ -4,7 +4,7 @@ import de.leonm.adventofcode21.utils.Reader;
 
 public abstract class Day {
 
-    Reader reader = new Reader();
+    protected Reader reader = new Reader();
 
     abstract int partOne();
     abstract int partTwo();
@@ -16,7 +16,7 @@ public abstract class Day {
         System.out.printf("Part one solution: %1$d \n\tTime required: %2$d ms.\n",
             solution, (prePartTwo - prePartOne));
         solution = partTwo();
-        long postPartTwo = System.currentTimeMillis();;
+        long postPartTwo = System.currentTimeMillis();
         System.out.printf("Part two solution: %1$d \n\tTime required: %2$d ms.\n",
             solution, (postPartTwo - prePartTwo));
     }
